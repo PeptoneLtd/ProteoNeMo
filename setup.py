cced56630a275d2d56a3e59abedccd74f89aff6f
Carlo Fisicaro <carlo@peptone.io>
2022-03-07 18:36:40 +0100

    build: add pip info

--- setup.py
+++ setup.py
@@ -12,9 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Setup for pip package."""
+
 import setuptools
 from setuptools import setup
 import importlib.util
+import codecs
+import os
 
 spec = importlib.util.spec_from_file_location('package_info', 'proteonemo/package_info.py')
 package_info = importlib.util.module_from_spec(spec)
@@ -31,20 +35,97 @@ __package_name__ = package_info.__package_name__
 __repository_url__ = package_info.__repository_url__
 __version__ = package_info.__version__
 
-with open("README.md") as f:
-    readme = f.read()
+if os.path.exists('README.md'):
+    with open("README.md", "r", encoding='utf-8') as fh:
+        long_description = fh.read()
+    long_description_content_type = "text/markdown"
 
-setup(
-    name="proteonemo",
+elif os.path.exists('README.rst'):
+    # codec is used for consistent encoding
+    long_description = codecs.open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst'), 'r', encoding='utf-8',
+    ).read()
+    long_description_content_type = "text/x-rst"
+
+else:
+    long_description = 'See ' + __homepage__
+
+
+###############################################################################
+#                             Dependency Loading                              #
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+
+def req_file(filename, folder="requirements"):
+    with open(os.path.join(folder, filename), encoding='utf-8') as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters
+    # Example: `\n` at the end of each line
+    return [x.strip() for x in content]
+
+
+install_requires = req_file("requirements.txt")
+
+###############################################################################
+
+
+setuptools.setup(
+    name=__package_name__,
     version=__version__,
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    # The project's main homepage.
+    url=__repository_url__,
+    download_url=__download_url__,
+    # Author details
+    author=__contact_names__,
+    author_email=__contact_emails__,
+    # maintainer Details
+    maintainer=__contact_names__,
+    maintainer_email=__contact_emails__,
+    # The licence under which the project is released
+    license=__license__,
+    classifiers=[
+        # How mature is this project? Common values are
+        #  1 - Planning
+        #  2 - Pre-Alpha
+        #  3 - Alpha
+        #  4 - Beta
+        #  5 - Production/Stable
+        #  6 - Mature
+        #  7 - Inactive
+        'Development Status :: 5 - Production/Stable',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        # Indicate what your project relates to
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Biophysics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+        # Supported python versions
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        # Additional Setting
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ],
     packages=setuptools.find_packages(),
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/PeptoneInc/ProteoNeMo",
-    license="Apache License, Version 2.0",
-    author="Peptone Ltd.",
-    author_email="carlo@peptone.io",
-    description=" Ongoing research training transformer models on proteome at scale",
-    data_files=[(".", ["LICENSE", "README.md", "CHANGELOG.md", "CITATION.cff"])],
-    zip_safe=True,
+    install_requires=install_requires,
+    # Add in any packaged data.
+    include_package_data=True,
+    package_data={'': ['*.tsv', '*.txt', '*.far', '*.fst', '*.cpp', 'Makefile', '*.fasta']},
+    zip_safe=False,
+    # PyPI package information.
+    keywords=__keywords__,
 )
