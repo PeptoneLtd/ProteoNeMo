119a28d8e4c5f3a893eaa9903b4e4598137436ce
cellini-tone <istvan@peptone.io>
2022-03-14 16:24:36 +0000

    add: added nemo representation parsing

--- downstreams/ssp.py
+++ downstreams/ssp.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+import re
 import numpy as np
 import torch
 import nemo
@@ -11,24 +13,37 @@ import csv
 from sklearn.model_selection import KFold
 from sklearn import linear_model
 import argparse
+import time
 
 
 args_parser = argparse.ArgumentParser(description='Get the path to the hdf5 files and the path for logging.')
 
 # Add the arguments
-args_parser.add_argument('data_path',
+args_parser.add_argument('-d',
+                        '--data_path',
                         action='store',
+                        required=True,
                         type=str
                         )
 
-args_parser.add_argument('repr_path',
+args_parser.add_argument('-r',
+                        '--repr_path',
                         action='store',
+                        required=True,
+                        type=str
+                        )
+args_parser.add_argument('-m',
+                        '--model',
+                        action='store',
+                        required=True,
                         type=str
                         )
 
-args_parser.add_argument('logging_folder',
-                        action='store',
+args_parser.add_argument('-log',
+                        '--logging_folder',
+                        help='See if this works...',
                         type=str
+                        #action='store_true'
                         )
 
 
@@ -36,9 +51,10 @@ args_parser.add_argument('logging_folder',
 args = args_parser.parse_args()
 cb513_data_path = args.data_path
 repr_path = args.repr_path
+model_name=args.model
 logging_folder_path = args.logging_folder
 
-def input_factory_from_hdf5(path_hdf5_file, path_repr):
+def input_factory_from_hdf5(path_hdf5_file, path_repr, model='nemo'):
     exes = []
     embeddings = {}
 
@@ -53,7 +69,10 @@ def input_factory_from_hdf5(path_hdf5_file, path_repr):
         if pdbid in embeddings.keys():
             embedding = embeddings[pdbid][pos].numpy()
         else:
-            embeddings[pdbid] = torch.load(f'{path_repr}{pdbid}.pt')['representations'][33].clone().cpu().detach()
+            if model=='nemo':
+                embeddings[pdbid] = torch.load(f'{path_repr}bert_reprs_{pdbid}.pt').clone().cpu().detach()
+            else:
+                embeddings[pdbid] = torch.load(f'{path_repr}{pdbid}.pt')['representations'][33].clone().cpu().detach()
             embedding = embeddings[pdbid][pos].numpy()
 
         exes.append(embedding)
@@ -86,7 +105,7 @@ def _file_checks(f_names):
     return True
 
 
-def main(path_folds, path_repr_folder, logging_path):
+def main(path_folds, path_repr_folder, *logging_path, model='nemo'):
     f_names = next(os.walk(f'{path_folds}'))[2]
     f_names = [file for file in f_names if file.endswith(".hdf5")]
 
@@ -108,8 +127,8 @@ def main(path_folds, path_repr_folder, logging_path):
                     test_file = x
 
         # read and prepare the representation data for regression
-        ex_train, zed_train = input_factory_from_hdf5(f'{path_folds}/{training_file}', path_repr_folder)
-        ex_test, zed_test = input_factory_from_hdf5(f'{path_folds}/{test_file}', path_repr_folder)
+        ex_train, zed_train = input_factory_from_hdf5(f'{path_folds}/{training_file}', path_repr_folder, model=model)
+        ex_test, zed_test = input_factory_from_hdf5(f'{path_folds}/{test_file}', path_repr_folder, model=model)
 
         lm = linear_model.LogisticRegression(multi_class='ovr', solver='liblinear')
         lm.fit(ex_train, zed_train)
@@ -122,12 +141,27 @@ def main(path_folds, path_repr_folder, logging_path):
     _std = np.std(list(res.values()))
     res['mean'], res['std'] = _mean, _std
 
-    with open(f'{logging_path}', 'w') as f:
-        w = csv.DictWriter(f, res.keys())
-        w.writeheader()
-        w.writerow(res)
+    if logging_path:
+        with open(f'{logging_path[0]}', 'w') as f:
+            w = csv.DictWriter(f, res.keys())
+            w.writeheader()
+            w.writerow(res)
 
-    f.close()
+        f.close()
+        print(f'Results are saved in {logging_path[0]}')
+
+    else:
+        if not os.path.isdir(f'{os.getcwd()}/tmp-downstream/'):
+            os.mkdir(f'{os.getcwd()}/tmp-downstream')
+
+        f_name_tag = f'{model}-SSP-'+time.strftime('%Y%m%d-%H%M%S')
+        with open(f'{os.getcwd()}/tmp-downstream/{f_name_tag}.csv', 'w') as f:
+            w = csv.DictWriter(f, res.keys())
+            w.writeheader()
+            w.writerow(res)
+
+        f.close()
+        print(f'Results are saved in {os.getcwd()}/tmp-downstream/{f_name_tag}.csv')
 
 if __name__ == "__main__":
-    main(cb513_data_path, repr_path, logging_folder_path)
+    main(cb513_data_path, repr_path, logging_folder_path, model=model_name)
